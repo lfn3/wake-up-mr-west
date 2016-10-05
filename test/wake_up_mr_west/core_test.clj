17b0c89a32cb5cb4d51fd4ee7662960e1c198930
lfn3 <liam@lfn3.net>
2016-10-05 22:52:15 +1300

    Test xml gen and call triggering

--- test/wake_up_mr_west/core_test.clj
+++ test/wake_up_mr_west/core_test.clj
@@ -1,7 +1,15 @@
 (ns wake-up-mr-west.core-test
   (:require [clojure.test :refer :all]
             [wake-up-mr-west.core :refer :all]
-            [clojure.core.async :as a]))
+            [clojure.core.async :as a]
+            [clojure.xml :as xml]
+            [clojure.string :as str])
+  (:import (java.io ByteArrayInputStream)
+           (java.util Date)))
+
+(use-fixtures :once #(do (reset! state {})
+                         (%1)
+                         (reset! state {})))
 
 (deftest go-with-retries-test
   (testing "Default counter"
@@ -25,3 +33,26 @@
           val 1]
       (a/put! ch val)
       (is (= val (a/<!! (go-with-retries (a/<! ch))))))))
+
+(defn parse-xml-str [s]
+  (xml/parse (ByteArrayInputStream. (.getBytes s))))
+
+(deftest say-test
+  (testing "Returns valid xml"
+    (is (parse-xml-str (say "Wake up Mr. West"))))
+  (testing "Contrains target string"
+    (let [target-str "Wake up Mr. West"]
+      (is (= target-str
+             (-> (parse-xml-str (say target-str))
+                 :content
+                 (first)
+                 :content
+                 (first)
+                 (str/trim)))))))
+
+(deftest add-test
+  (testing "Makes the call"
+    (let [made-call? (a/promise-chan)]
+      (with-redefs [make-call (fn [& _] (a/put! made-call? true))]
+        (add (Date.) "" "")
+        (is (a/<!! made-call?))))))
